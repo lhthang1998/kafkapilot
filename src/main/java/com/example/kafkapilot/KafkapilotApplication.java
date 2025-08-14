2f790fff4f2c732d0e4f07dd99bfea504cae7ac9
Lê Hữu Thắng <lhthang.1998@gmail.com>
2025-08-14 22:18:31 +0700

    first commit

---
+++ src/main/java/com/example/kafkapilot/KafkapilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkapilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkapilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkapilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkapilot/KafkapilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkapilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkapilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
