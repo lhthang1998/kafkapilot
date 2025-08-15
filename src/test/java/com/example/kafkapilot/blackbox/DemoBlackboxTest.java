046d83f3d886c3406674cacbc8316eaa490dfa9b
Lê Hữu Thắng <lhthang.1998@gmail.com>
2025-08-15 07:27:13 +0700

    done component test

---
+++ src/test/java/com/example/kafkapilot/blackbox/DemoBlackboxTest.java
@@ -0,0 +1,2 @@
+package com.example.kafkapilot.blackbox;public class DemoBlackboxTest {
+}
--- src/test/java/com/example/kafkapilot/component/DemoComponentTest.java
+++ src/test/java/com/example/kafkapilot/component/DemoComponentTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class DemoComponentTest extends ComponentBase {
     @Test
     void testRedis() {
