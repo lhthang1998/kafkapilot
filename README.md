5c7074ad812f673bfbf9e0c6613ef6b320540e36
Lê Hữu Thắng <lhthang.1998@gmail.com>
2025-08-16 09:34:44 +0700

    done create topic base

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 https://github.com/avast/gradle-docker-compose-plugin
 
 ## Kafka with KRAFT
+
+## Schema Registry API
+https://docs.confluent.io/platform/current/schema-registry/develop/api.html#subjects
+
--- src/main/resources/docker/script.py
+++ src/main/resources/docker/script.py
@@ -1,6 +1,10 @@
 import json
 import logging
+import os
+from pathlib import Path
+
 import requests as requests
+import yaml
 
 REST_PROXY_URL = "http://localhost:8082/v3"
 SCHEMA_REGISTRY_URL = "http://localhost:8081"
@@ -24,10 +28,11 @@ def get_cluster_id():
 cluster = get_cluster_id()
 
 
-def create_topic(topic):
+def create_topic(topic, config):
+    partitions_count = config['topic']['partitions']
     topic_resp = requests.post(url=f'{REST_PROXY_URL}/clusters/{cluster}/topics', headers=HEADERS, json={
         "topic_name": topic,
-        "partitions_count": 3,
+        "partitions_count": partitions_count,
         "replication_factor": 1,
         "configs": [
             {
@@ -36,24 +41,26 @@ def create_topic(topic):
             }
         ]
     })
-
-    create_key_schema(topic)
+    create_schema(f'{topic}-key', config['subjects'][f'{topic}-key']['schema'])
+    create_schema(f'{topic}-value', config['subjects'][f'{topic}-value']['schema'])
     print(f'Creating topic: {topic} -> {get_result(topic_resp)}')
 
 
-def create_key_schema(topic):
-    resp = requests.post(url=f'http://localhost:8081/subjects/{topic}-key/versions', headers=HEADERS, json={
-        "schema": json.dumps({"type": "string"})
+def create_schema(schema_name, schema_body):
+    resp = requests.post(url=f'http://localhost:8081/subjects/{schema_name}/versions', headers=HEADERS, json={
+        "schema": schema_body
     })
-    return resp
-
-
-def create_value_schema(topic):
-    resp = requests.post(url=f'http://localhost:8081/subjects/{topic}-value/versions', headers=HEADERS, json={
-        "schema": json.dumps({"type": "string"})
+    requests.put(url=f'http://localhost:8081/config/{schema_name}', headers=HEADERS, json={
+        "compatibility": "FORWARD_TRANSITIVE"
     })
     return resp
 
 
 if __name__ == '__main__':
-    create_topic('test')
+    directory = Path('./topics')
+    for item in directory.iterdir():
+        print(item.name)
+        with open(item, 'r') as file:
+            config = yaml.safe_load(file)
+            topic_name = config['topic']['name']
+            create_topic(topic_name, config)
